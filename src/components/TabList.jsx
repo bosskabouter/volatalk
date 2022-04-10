7dea041a5f55c649c5e3def87c954bc74786ec1c
Paul Kuit <bosskabouter@protonmail.com>
2022-04-10 16:48:15 +0200

    Initial commit

---
+++ src/components/TabList.jsx
@@ -0,0 +1,7 @@
+import('./index') 
+
+const tablist = 
+React.createElement('div', {className: 'shopping-list'},
+  React.createElement('h1', /* ... h1 children ... */),
+  React.createElement('ul', /* ... ul children ... */)
+);
