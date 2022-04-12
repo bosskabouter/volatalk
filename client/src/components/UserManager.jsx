49e141703eec2852f548747fe8d251e4f93b592b
Paul Kuit <bosskabouter@protonmail.com>
2022-04-12 16:16:49 +0200

    VolaTalk Server initial commit

--- client/src/components/UserManager.jsx
+++ client/src/components/UserManager.jsx
@@ -2,25 +2,31 @@ import { identicon } from "minidenticons";
 
 import React from "react";
 
-
-
 export default class UserManager extends React.Component {
   user = { nickname: "Paul2", id: "297931" };
-  
-  constructor(props) {
-    super(props);
-    identicon(this.user.id);
+
+  constructor() {
+    super();
+    this.state = {
+      user: this.user,
+    };
+
+    this.iconStr = identicon(this.user.id);
+    console.info("iconStr " + this.iconStr);
   }
   render() {
     return (
       <div>
         <span>{this.user.nickname}</span>
-        <identicon-svg username={this.user.id} />
+        <span style={{ width: "20px" }}>
+          <img
+            src={`data:image/svg+xml;utf8,${this.iconStr}`}
+            alt={"Your personsal identification icon"}
+          />
+        </span>
       </div>
     );
   }
 }
 
-export class User{
-
-}
+export class User {}
