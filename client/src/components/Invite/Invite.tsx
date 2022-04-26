38ccd8f40a98f1c0fe4d603591eaf3db400380d3
Paul Kuit <bosskabouter@protonmail.com>
2022-04-26 12:37:11 +0200

    Invite in Dialog

--- client/src/components/Invite/Invite.tsx
+++ client/src/components/Invite/Invite.tsx
@@ -1,25 +1,30 @@
+import { checkReceivedInvite, makeInvite } from 'services/InvitationService';
+
 import React, { useContext, useRef } from 'react';
 
 import ToggleButton from '@mui/material/ToggleButton';
 import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
+import { Link } from 'react-router-dom';
+
 import { QRCodeSVG } from 'qrcode.react';
 import { QrReader } from 'react-qr-reader';
-import { checkReceivedInvite, makeInvite } from 'services/InvitationService';
+
 import shareSomething from 'util/Share';
 import { Box, Button } from '@mui/material';
 import { UserContext } from 'providers/UserProvider';
 import TextField from '@mui/material/TextField';
 
-import QrCode2Icon from '@mui/icons-material/QrCode2';
+import QrCode2Icon from '@mui/icons-material/QrCode';
 import QrCodeScannerIcon from '@mui/icons-material/QrCodeScanner';
+import Dialog from '@mui/material/Dialog';
+
+import Close from '@mui/icons-material/Close';
 
 export default function Invite() {
   const inputRef = useRef();
   const { user } = useContext(UserContext);
 
   const [showOrScan, setShowOrScan] = React.useState<boolean>(true);
-  // const [inviteTxt, setInviteTxt] = React.useState('');
-
 
   const handleShowOrScan = (_event: React.MouseEvent<HTMLElement>, newScanOrShow: boolean) => {
     setShowOrScan(newScanOrShow);
@@ -39,19 +44,18 @@ export default function Invite() {
         return;
       }
       const inviteText = event.target.value;
-  
+
       console.debug('Invitation text: ' + inviteText);
       makeInvite(user, inviteText).then((iUrl) => {
         console.log('Generated new inviteURL: ' + iUrl);
         setInviteUrl(iUrl);
-        //setInviteTxt(inviteText);
-        //inputRef?.current?.focus();
       });
     };
-  
+
     return (
       <>
         <Box>
+          <br />
           <TextField
             spellCheck
             inputRef={inputRef}
@@ -71,7 +75,11 @@ export default function Invite() {
             Share Invite!
           </Button>
           <br />
-          <br /> <QRCodeSVG value={inviteUrl} size={300} includeMargin style={{ width: '100%' }} />
+          <br />
+
+          {inviteUrl.length > 0 && (
+            <QRCodeSVG value={inviteUrl} size={300} includeMargin style={{ width: '100%' }} />
+          )}
         </Box>
       </>
     );
@@ -95,24 +103,29 @@ export default function Invite() {
           }}
           constraints={{ noiseSuppression: true }}
         />
-        <p>{scanResult}</p>
+        <em>{scanResult}</em>
       </>
     );
   };
 
   return (
-    <>
-      {showOrScan ? <DisplayQR /> : <DisplayScanner />}
+    <Dialog open fullWidth>
+      <Link to="/">
+        <Close />
+      </Link>
+      <>
+        {showOrScan ? <DisplayQR /> : <DisplayScanner />}
 
-      <ToggleButtonGroup value={showOrScan} fullWidth exclusive onChange={handleShowOrScan}>
-        <ToggleButton value={true}>
-          <QrCode2Icon /> Show Your Invite
-        </ToggleButton>
-        <ToggleButton value={false}>
-          Read someone&apos;s Invite
-          <QrCodeScannerIcon />
-        </ToggleButton>
-      </ToggleButtonGroup>
-    </>
+        <ToggleButtonGroup value={showOrScan} fullWidth exclusive onChange={handleShowOrScan}>
+          <ToggleButton value={true}>
+            <QrCode2Icon /> SHOW INVITE
+          </ToggleButton>
+          <ToggleButton value={false}>
+            READ INVITE
+            <QrCodeScannerIcon />
+          </ToggleButton>
+        </ToggleButtonGroup>
+      </>
+    </Dialog>
   );
 }
